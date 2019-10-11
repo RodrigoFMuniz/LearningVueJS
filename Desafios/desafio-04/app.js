5d45a152573bf5c698e044601e6071283fdb5fee
Rodrigo Muniz <rodrigo.muniz.dev@gmail.com>
2019-10-10 23:40:01 -0300

    Finalização do módulo 01

--- Desafios/desafio-04/app.js
+++ Desafios/desafio-04/app.js
@@ -2,16 +2,43 @@ new Vue({
 	el: '#desafio',
 	data: {
 		aplicarEfeito: false,
-		classe1: 'destaque'
+		classe1: 'destaque',
+		perigo: true,
+		classe3: '',
+		classe4: '',
+		classe5: false,
+		cor: 'red',
+		largura: 0,
+		altura: 30
 	},
 	methods: {
 		iniciarEfeito() {
 			setInterval(()=>{
-				this.classe1 = this.classe1 == 'destaque'?'encolher':'destaque'
+				this.classe1 = this.classe1 == 'destaque'? 'encolher':'destaque'
 			}, 1000)
 		},
 		iniciarProgresso() {
-
+			setInterval(()=>{
+				if(this.largura < 300){
+					this.largura += 3
+				}
+			},10)
+		},
+		setPerigo(ev){
+			if(ev.target.value == "true"){
+				this.classe5 = true
+			}else if(ev.target.value == "false"){
+				this.classe5 = false
+			}
+		}
+	},
+	computed:{
+		meuEstilo(){
+			return{
+				backgroundColor: this.cor,
+				width: this.largura + 'px',
+				height: this.altura +'px'
+			}
 		}
 	}
 })
