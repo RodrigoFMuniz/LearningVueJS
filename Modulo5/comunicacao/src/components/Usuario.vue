ddc44ca774a1e257cc7116b7b736e46dd0719b08
Rodrigo Muniz <rodrigo.muniz.dev@gmail.com>
2019-11-14 22:31:11 -0300

    Working with props

--- Modulo5/comunicacao/src/components/Usuario.vue
+++ Modulo5/comunicacao/src/components/Usuario.vue
@@ -2,14 +2,17 @@
     <div class="container">
         <h1>Componente Usuário</h1>
         <p>Esse é um componente muito legal!</p>
+        <p>Nome é <strong>{{nome}}</strong></p>
         <button @click="alterarNome">Alterar Nome</button>
         <hr>
         <div class="componentes">
-            <app-usuario-info :nome='usuario' />
+            <app-usuario-info :nome='nome' @nomeMudou="nome=$event.old"/>
             <app-usuario-editar />
         </div>
     </div>
 </template>
+
+
 
 <script>
 import AppUsuarioInfo from './UsuarioInfo'
@@ -19,19 +22,25 @@ export default {
     components: { AppUsuarioInfo, AppUsuarioEditar },
     data(){
         return {
-            usuario:{
-                nome:'Pedro',
-                sobrenome: 'qualquer'
-            }
+            nome: 'Rodrigo'
+            // usuario:{
+            //     nome:'Rodrigo',
+            //     sobrenome: 'qualquer',
+            //     idade: 20
+            // }
         }
     },
     methods:{
         alterarNome(){
-            this.usuario.nome='Ana'
+            this.nome='Ana'
         }
     }
 }
 </script>
+
+
+
+
 
 <style scoped>
     .container {
--- Modulo5/comunicacao/src/components/UsuarioInfo.vue
+++ Modulo5/comunicacao/src/components/UsuarioInfo.vue
@@ -2,22 +2,50 @@
     <div class="componente">
         <h2>As Informações de Usuário</h2>
         <p>Vários detalhes...</p>
-        <p>Nome do usuário: <strong>{{nome.nome}}</strong></p>
-        {{inverterNome()}}
+        <p>Nome do usuário: <strong>{{nome}}</strong></p>
+        <button @click="reiniciarNome">Reiniciar nome</button>
     </div>
 </template>
 
 <script>
 export default {
-    props:['nome'],
-    data(){
-        return{
-            nomeAlternativo: this.nome
-        }
+    // props:['nome'],
+    // data(){
+    //     return{
+    //         nomeAlternativo: this.nome
+    //     }
+    // },
+    // Outra forma de declarar props
+    props:{
+        nome:{
+            type: String,
+            default: 'Anonimo'
+        } 
     },
+    // inserindo atributos
+    // props:{
+    //     nome:{
+    //         type: Object,
+    //         nome:{
+    //             type: String,
+    //             default: 'Anônimo'
+
+    //         }
+    //         // required: true,
+    //     }
+
+    // },
     methods:{
         inverterNome(){
-            return this.nome.nome.split('').reverse().join("")
+            return this.nome.split('').reverse().join("")
+        },
+        reiniciarNome(){
+            const old = this.nome
+            this.nome='Pedro'
+            this.$emit('nomeMudou', {
+                new: this.nome,
+                old
+                })
         }
     }
     
